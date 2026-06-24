03dedd41b0b2246f323e859e26163d1725cbc5c6
Yash Mangla <yashmangla.official@gmail.com>
2026-06-24 14:55:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.16%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while(x!=0){
+        if ((rev > INT_MAX/10) || (rev == INT_MAX/10 && (x%10)>7)) return 0;
+        if ((rev < INT_MIN/10) || (rev == INT_MIN/10 && (x%10)<-8)) return 0;
+        
+        else{
+            rev = (rev*10) + x%10;
+            x /= 10;
+            }
+        }
+
+        return rev;
+    }
+};
