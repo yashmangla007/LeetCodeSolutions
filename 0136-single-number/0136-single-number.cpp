59f0e75b702751a02046bf5228bba7856854bf61
Yash Mangla <yashmangla.official@gmail.com>
2026-07-02 22:14:43 +0530

    Time: 0 ms (100%), Space: 20.8 MB (13.47%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xorr=0;
+        for(int i =0; i<n; i++){
+            xorr = nums[i]^xorr;
+        }
+        return xorr;
+    }
+};
