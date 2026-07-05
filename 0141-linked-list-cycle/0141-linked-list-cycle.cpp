3b8893de3cc6d68017cb8b989cb11c4300c31126
Yash Mangla <yashmangla.official@gmail.com>
2026-07-05 13:54:18 +0530

    Time: 3 ms (99.53%), Space: 11.7 MB (81.31%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -7,20 +7,18 @@
  * };
  */
 
- //Fun Solution (constraint based):
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==nullptr || head->next == nullptr) return false;
         ListNode* temp = head;
-        int count = 1;
-        while(temp!=nullptr){
-            if (count > 10000) return true;
-            count++;
-            temp = temp->next;
-        }
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while(fast!= nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) return true;
+        }
         return false;
     }
 };
